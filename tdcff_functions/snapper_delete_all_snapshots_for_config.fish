f2a7eb11d335171495ad7c98a5a797859375d008
raldone01 <raldone01@gmail.com>
2023-11-20 13:39:52 +0100

    Add snapper script to delete all snapshots of a specific config.

---
+++ tdcff_functions/snapper_delete_all_snapshots_for_config.fish
@@ -0,0 +1,17 @@
+#!/bin/fish
+function tdc_snapper_delete_all_snapshots_for_config --description "Deletes all snapshots for the selected config." --argument config
+    if test -z "$config"
+        printf "Usage: tdc_snapper_delete_all_snapshots_for_config <config_name>\n"
+        return 1
+    end
+
+    set snapshot_numbers "$(sudo snapper --no-headers --csvout --config "$config" list --columns number | tail -n +2)"
+
+    for i in $snapshot_numbers;
+        sudo snapper -c "$config" delete $i;
+    end
+end
+
+if not string match -q -- "*from sourcing file*" (status)
+    tdc_snapper_delete_all_snapshots_for_config $argv
+end
