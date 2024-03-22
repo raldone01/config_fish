c3a7bcd4ac27e1b88635b7868cecc96fae41447e
raldone01 <raldone01@gmail.com>
2024-03-22 13:12:29 +0100

    Add zoxide to deps.

--- tdcff_functions/setup_deps.fish
+++ tdcff_functions/setup_deps.fish
@@ -12,7 +12,7 @@ function tdc_setup_deps --description "Installs/Reinstalls the dependencies of t
     echo "cargo not installed"
     return 1
   end
-  yay -Sy fish rustup keychain figlet boxes kubectl rsync
+  yay -Sy fish rustup keychain figlet boxes kubectl rsync zoxide
   rustup install stable
   cargo install viu exa
 end
