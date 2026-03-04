2cba44c819dd67e12129f06027daae13b6bc0bd6
raldone01 <raldone01@gmail.com>
2026-03-04 03:32:43 +0100

    Support chafa.

--- helpers/advanced_greeting.fish
+++ helpers/advanced_greeting.fish
@@ -1,95 +1,114 @@
 if test -z "$image_viewer"
-  set -g image_viewer tiv
+    if type -q chafa
+        set -g image_viewer chafa
+    else if type -q tiv
+        set -g image_viewer tiv
+    else if type -q viu
+        set -g image_viewer viu
+    end
 end
 
 # Returns a random image path in the $pic_folders directory.
 # If no image is found or there was an error it returns nothing.
 # If an image path is returned the function pic_not_nice is defined.
 function rand_pic_file
-  if test -z "$pic_folders"
-    set -g last_pic_file ""
-  else
-    set -g last_pic_file (find $pic_folders -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png \)  2> /dev/null | shuf -n 1)
-  end
-
-  if test -r "$last_pic_file"
-    # check if the file is readable
-
-    printf "%s" "$last_pic_file"
-
-    function pic_not_nice
-      if test -z "$last_pic_file"
-        echo "Last picture not found!"
-        return
-      end
-      rm -i "$last_pic_file"
-      functions -e pic_not_nice
+    if test -z "$pic_folders"
+        set -g last_pic_file ""
+    else
+        set -g last_pic_file (find $pic_folders -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png \)  2> /dev/null | shuf -n 1)
     end
-  end
+
+    if test -r "$last_pic_file"
+        # check if the file is readable
+
+        printf "%s" "$last_pic_file"
+
+        function pic_not_nice
+            if test -z "$last_pic_file"
+                echo "Last picture not found!"
+                return
+            end
+            rm -i "$last_pic_file"
+            functions -e pic_not_nice
+        end
+    end
 end
 
 function calm
-  if not type -q $image_viewer
-    echo "No image viewer found!"
-    return
-  end
-  set -l rand_pic_file (rand_pic_file)
-  if test -z "$rand_pic_file"
-    echo "No pictures found!"
-    return
-  end
-  set -l filename (basename $rand_pic_file)
-  echo "Featured pic $filename (Run pic_not_nice to delete)"
-  image_viewer $argv $rand_pic_file
+    if not type -q $image_viewer
+        echo "No image viewer found!"
+        return
+    end
+    set -l rand_pic_file (rand_pic_file)
+    if test -z "$rand_pic_file"
+        echo "No pictures found!"
+        return
+    end
+    set -l filename (basename $rand_pic_file)
+    echo "Featured pic $filename (Run pic_not_nice to delete)"
+    $image_viewer $argv $rand_pic_file
 end
 
 function fish_greeting
-  set -l rand_pic_file (rand_pic_file)
-  set -l terminal_height (tput lines)
-  set -l terminal_width (tput cols)
+    function __show_image -S
+        # takes three arguments: image path, desired width, desired height
+        set -l image_path $argv[1]
+        set -l desired_width $argv[2]
+        set -l desired_height $argv[3]
 
-  function __before_pic_textrivate_mode -S
-    # print login message
-
-    if test $terminal_width -gt 134;
-      or test $terminal_height -lt 53
-      #print it in one line
-      set date_str (date +"%d/%b/%Y %H:%M:%S")
-    else
-      # print it in two lines
-      set date_str "$(printf "%s\n%s" (date +"%d/%b/%Y") (date +"%H:%M:%S"))"
+        if test -n "$image_path"
+            and type -q $image_viewer
+            if test "$image_viewer" = chafa
+                $image_viewer --size "$desired_width"x"$desired_height" "$image_path"
+            else if contains -- "$image_viewer" viu tiv
+                $image_viewer -w "$desired_width" -h "$desired_height" "$image_path"
+            end
+        end
     end
 
-    if type -q boxes
-      and test $terminal_width -gt 77;
-      and test $terminal_height -gt 40
-      printf "%s" $date_str | figlet -t | boxes -d scroll
-    else
-      printf "%s\n" $date_str
+    set -l rand_pic_file (rand_pic_file)
+    set -l terminal_height (tput lines)
+    set -l terminal_width (tput cols)
+
+    function __before_pic_textrivate_mode -S
+        # print login message
+
+        if test $terminal_width -gt 134;
+            or test $terminal_height -lt 53
+            #print it in one line
+            set date_str (date +"%d/%b/%Y %H:%M:%S")
+        else
+            # print it in two lines
+            set date_str "$(printf "%s\n%s" (date +"%d/%b/%Y") (date +"%H:%M:%S"))"
+        end
+
+        if type -q boxes
+            and test $terminal_width -gt 77;
+            and test $terminal_height -gt 40
+            printf "%s" $date_str | figlet -t | boxes -d scroll
+        else
+            printf "%s\n" $date_str
+        end
+
+        echo "Use calm to calm. Powered by fish the friendly interactive shell."
+        if test -n "$rand_pic_file"
+            and type -q $image_viewer
+            set -l filename (basename $rand_pic_file)
+            echo "Featured pic $filename (Run pic_not_nice to delete)"
+        end
     end
-
-    echo "Use calm to calm. Powered by fish the friendly interactive shell."
-    if test -n "$rand_pic_file"
-      and type -q $image_viewer
-      set -l filename (basename $rand_pic_file)
-      echo "Featured pic $filename (Run pic_not_nice to delete)"
+    function __after_pic_text -S
     end
-  end
-  function __after_pic_text -S
-  end
-  set -l before_pic_text (__before_pic_textrivate_mode)
-  set -l after_pic_text (__after_pic_text)
+    set -l before_pic_text (__before_pic_textrivate_mode)
+    set -l after_pic_text (__after_pic_text)
 
-  set -l before_lines (printf "%s\n" $before_pic_text | wc -l)
-  set -l after_lines (printf "%s\n" $after_pic_text | wc -l)
-  set -l prompt_lines (printf "%s\n" $fish_prompt | wc -l)
+    set -l before_lines (printf "%s\n" $before_pic_text | wc -l)
+    set -l after_lines (printf "%s\n" $after_pic_text | wc -l)
+    set -l prompt_lines (printf "%s\n" $fish_prompt | wc -l)
 
-  # echo "$terminal_height $before_lines $after_lines $prompt_lines"
-  set -l image_height (math $terminal_height - $before_lines - $after_lines - $prompt_lines)
-  printf "%s\n" $before_pic_text
-  if test -n "$rand_pic_file"
-    and type -q $image_viewer
-    image_viewer -h "$image_height" -w "$terminal_width" "$rand_pic_file"
-  end
-  printf "%s" $after_pic_text
+    # echo "$terminal_height $before_lines $after_lines $prompt_lines"
+    set -l image_height (math $terminal_height - $before_lines - $after_lines - $prompt_lines)
+    printf "%s\n" $before_pic_text
+    __show_image $rand_pic_file $terminal_width $image_height
+    printf "%s" $after_pic_text
 end
--- tdcff_functions/setup_deps.fish
+++ tdcff_functions/setup_deps.fish
@@ -1,22 +1,21 @@
 #!/bin/fish
 function tdc_setup_deps --description "Installs/Reinstalls the dependencies of tdc."
-  if not type -q yay
-    echo "yay not installed"
-    return 1
-  end
-  if not type -q rustup
-    echo "rustup not installed"
-    return 1
-  end
-  if not type -q cargo
-    echo "cargo not installed"
-    return 1
-  end
-  yay -Sy fish rustup keychain figlet boxes kubectl rsync zoxide eza
-  rustup install stable
-  cargo install viu
+    if not type -q yay
+        echo "yay not installed"
+        return 1
+    end
+    if not type -q rustup
+        echo "rustup not installed"
+        return 1
+    end
+    if not type -q cargo
+        echo "cargo not installed"
+        return 1
+    end
+    yay -Sy --needed fish rustup keychain figlet boxes kubectl rsync zoxide eza chafa
+    rustup install stable
 end
 
 if not string match -q -- "*from sourcing file*" (status)
-  tdc_setup_deps $argv
+    tdc_setup_deps $argv
 end
